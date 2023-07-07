7c67a826aa642baf551ef135311f8eb32c4557c1
MichałM <michalhm@gmail.com>
2023-07-07 23:58:59 +0200

    Decimal Count Check Function

--- main.py
+++ main.py
@@ -13,18 +13,18 @@ Program po uruchomieniu wyświetla informację o dostępnych komendach:
 
 
 Po wprowadzeniu odpowiedniej komendy, aplikacja zachowuje się w unikalny sposób dla każdej z nich:
-1. saldo - Program pobiera kwotę do dodania lub odjęcia z konta.
+***DONE*** 1. saldo - Program pobiera kwotę do dodania lub odjęcia z konta.
 2. sprzedaż - Program pobiera nazwę produktu, cenę oraz liczbę sztuk. Produkt musi znajdować się w magazynie. 
 Obliczenia respektuje względem konta i magazynu (np. produkt "rower" o cenie 100 i jednej sztuce spowoduje odjęcie z magazynu produktu "rower" oraz dodanie do konta kwoty 100).
 3. zakup - Program pobiera nazwę produktu, cenę oraz liczbę sztuk. Produkt zostaje dodany do magazynu, jeśli go nie było. Obliczenia są wykonane odwrotnie do komendy "sprzedaz". 
 Saldo konta po zakończeniu operacji „zakup” nie może być ujemne.
-4. konto - Program wyświetla stan konta.
+***DONE*** 4. konto - Program wyświetla stan konta.
 5. lista - Program wyświetla całkowity stan magazynu wraz z cenami produktów i ich ilością.
 6. magazyn - Program wyświetla stan magazynu dla konkretnego produktu. Należy podać jego nazwę.
 7. przegląd - Program pobiera dwie zmienne „od” i „do”, na ich podstawie wyświetla wszystkie wprowadzone akcje zapisane pod indeksami od „od” do „do”. 
 Jeżeli użytkownik podał pustą wartość „od” lub „do”, program powinien wypisać przegląd od początku lub/i do końca. 
 Jeżeli użytkownik podał zmienne spoza zakresu, program powinien o tym poinformować i wyświetlić liczbę zapisanych komend (żeby pozwolić użytkownikowi wybrać odpowiedni zakres).
-8. koniec - Aplikacja kończy działanie.
+***DONE*** 8. koniec - Aplikacja kończy działanie.
 
 Dodatkowe wymagania:
 - Aplikacja od uruchomienia działa tak długo, aż podamy komendę "koniec".
@@ -133,17 +133,33 @@ def history_overview():
         print("*" * 30)
 
 
+'''3. zakup - Program pobiera nazwę produktu, cenę oraz liczbę sztuk. Produkt zostaje dodany do magazynu, jeśli go nie było. Obliczenia są wykonane odwrotnie do komendy "sprzedaz". 
+Saldo konta po zakończeniu operacji „zakup” nie może być ujemne.'''
+
+
+def decimal_count_check(number, message):
+    decimal_count = 3
+    while decimal_count > 2:
+        number = float(
+            input(message).replace(",", "."))
+        decimal_count = len(str(number).split(".")[1])
+        if decimal_count > 2:
+            bad_response()
+        else:
+            return number
+
+
+def buy():
+    item_name = str(input("Podaj nazwe przedmiotu: "))
+    cost_price = None
+
+
 def balance():
     amount = None
     while not isinstance(amount, float):
         try:
-            decimal_count = 3
-            while decimal_count > 2:
-                amount = float(
-                    input("Podaj kwote do dodania/odjecia z konta: ").replace(",", "."))
-                decimal_count = len(str(amount).split(".")[1])
-                if decimal_count > 2:
-                    bad_response()
+            balance_message = "Podaj kwote do dodania/odjecia z konta: "
+            amount = decimal_count_check(amount, balance_message)
             amount_confirm = confirm(amount)
             if amount_confirm == True:
                 if amount > 0:

82c75cc6599243e29ed73571fc65efde27b7054a
MichałM <michalhm@gmail.com>
2023-08-25 00:25:27 +0200

    Load / Save Beta

--- main.py
+++ main.py
@@ -1,6 +1,46 @@
-account_balance = 0
-history = []
-inventory = {}
+import json
+
+
+class FileHandler:
+    def __init__(self, history_file, balance_file, inventory_file):
+        self.history = self.load_history(history_file)
+        self.account_balance = self.load_balance(balance_file)
+        self.inventory = self.load_inventory(inventory_file)
+
+    def load_history(self, history_file):
+        with open(history_file, "r") as history_data:
+            history = history_data.readlines()
+        return history
+
+    def load_balance(self, balance_file):
+        with open(balance_file, "r") as balance_data:
+            account_balance = float(balance_data.readline())
+        return account_balance
+
+    def load_inventory(self, inventory_file):
+        with open(inventory_file, "r") as inventory_data:
+            inventory = json.load(inventory_data)
+        return inventory
+
+
+class FileWriter:
+    def __init__(self, history_file, balance_file, inventory_file):
+        self.history_file = history_file
+        self.balance_file = balance_file
+        self.inventory_file = inventory_file
+
+    def save_history(self, history):
+        with open(self.history_file, "w") as history_data:
+            for new_history_position in history:
+                history_data.write(new_history_position + "\n")
+
+    def save_balance(self, balance):
+        with open(self.balance_file, "w") as balance_data:
+            balance_data.write(str(balance))
+
+    def save_inventory(self, inventory):
+        with open(self, inventory) as inventory_data:
+            json.dump(inventory, inventory_data)
 
 
 def menu():
@@ -405,6 +445,13 @@ def inventory_correction():
 
 
 run = True
+load_data = FileHandler("history.txt", "balance.txt", "inventory.json")
+history = load_data.history
+account_balance = load_data.account_balance
+inventory = load_data.inventory
+print(history)
+print(account_balance)
+print(inventory)
 while run:
     command_check = True
     while command_check:
@@ -441,6 +488,11 @@ while run:
             elif command == 9:
                 command_check = False
                 run = False
+                save_data = FileWriter(
+                    "history.txt", "balance.txt", "inventory.json")
+                save_data.save_history(history)
+                save_data.save_balance(account_balance)
+                save_data.save_inventory(inventory)
             else:
                 bad_response()
         except ValueError:
